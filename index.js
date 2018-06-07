172c43f3a56b27da0ffa104697d268de826ce8c0
Manan Panchal <mananvpanchal@gmail.com>
2018-06-07 10:20:02 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const ArraySet = require('./src/array-set');
+const Functor = require('./src/functor');
+const Apply = require('./src/apply');
+const Applicative = require('./src/applicative');
+const Chain = require('./src/chain');
+const Monad = require('./src/monad');
+
+exports.default = Monad;
+
+exports.ArraySet = ArraySet;
+exports.Functor = Functor;
+exports.Apply = Apply;
+exports.Applicative = Applicative;
+exports.Chain = Chain;
